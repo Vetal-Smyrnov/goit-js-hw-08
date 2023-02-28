324a67bb293a8011588722e2f66ad09b86f6c99f
Vitalii Smyrnov <113116677+Vetal-Smyrnov@users.noreply.github.com>
2023-02-28 22:07:56 +0200

    init new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryItemsHtml = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div>
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </div>`
+  )
+  .join('');
+
+galleryEl.innerHTML = galleryItemsHtml;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
